6e30810d4032cf7be0e9329267bf9dd62376e112
haroon-ET <service-account@ehsaantech.com>
2024-01-19 20:02:40 +0500

    refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Movies from "./movies/page";
-import Login from "./login/page";
 
 export default function Home() {
-  return <Login/>;
+  return <Movies />;
 }
