4948d0da737889ea24071d7d06ddcb165ad8fc87
haroon-ET <service-account@ehsaantech.com>
2024-01-19 19:38:39 +0500

    fix landing redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Movies from "./movies/page";
+import Login from "./login/page";
 
 export default function Home() {
-  return <Movies />;
+  return <Login/>;
 }
